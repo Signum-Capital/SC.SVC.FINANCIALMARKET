4f1b7107c64276c1bdf044cc0c7baaf4279beb39
Pedro Martins <76529868+martinsbrz@users.noreply.github.com>
2021-07-14 01:15:51 -0300

    Expiração para catalogar

--- src/SC.FINANCIALMARKET/SC.FINANCIALMARKET.API/Areas/V2/Controllers/CatalogadorV2Controller.cs
+++ src/SC.FINANCIALMARKET/SC.FINANCIALMARKET.API/Areas/V2/Controllers/CatalogadorV2Controller.cs
@@ -6,7 +6,9 @@ using SC.FINANCIALMARKET.DOMAIN.Hubs;
 using SC.FINANCIALMARKET.DOMAIN.Services;
 using SC.INFRA.INFRAESTRUCTURE.Contexts;
 using SC.INFRA.INFRAESTRUCTURE.DB.FINANCIALMARKETDATA;
+using SC.INFRA.INFRAESTRUCTURE.Interfaces;
 using SC.PKG.SERVICES.Services;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,16 +18,29 @@ namespace SC.FINANCIALMARKET.API.Areas.V2.Controllers
     {
         private readonly IHubContext<CatalogadorHub> _hubContext;
         private FinancialMarketDataContext FinancialMarketDataContext { get; }
+        public IUsuarioPlataformaRepository UsuarioPlataformaRepository { get; }
 
-        public CatalogadorV2Controller(IHubContext<CatalogadorHub> hubContext, FinancialMarketDataContext financialMarketDataContext)
+        public CatalogadorV2Controller(IHubContext<CatalogadorHub> hubContext, FinancialMarketDataContext financialMarketDataContext, IUsuarioPlataformaRepository usuarioPlataformaRepository)
         {
             _hubContext = hubContext;
             FinancialMarketDataContext = financialMarketDataContext;
+            UsuarioPlataformaRepository = usuarioPlataformaRepository;
         }
 
         [HttpPost("Catalogar/realtime")]
         public async Task<IActionResult> Post(ConsultaRequest consultaRequest)
         {
+            var token = HeaderService.GetAuthorization(Request);
+            var user = TokenService.RevertToken(token);
+
+            var usuplat = await UsuarioPlataformaRepository.RecuperarPorUsuarioPlataformaAsync(user.UsuarioId, 1009);
+
+            if (usuplat == null)
+                return Result(null, "USER_NOT_FOUND", false);
+
+            else if (usuplat.Expiracao < DateTime.UtcNow)
+                return Result(null,"TIME_EXPIRED",false);
+
             var consulta = ConvertObject.Convert<Consulta>(consultaRequest);
             new CatalogerRealTimeFactory(consulta, consultaRequest.ConnectionId, _hubContext).Produce();
 

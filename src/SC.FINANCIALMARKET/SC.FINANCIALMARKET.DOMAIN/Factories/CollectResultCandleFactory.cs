5c74ccebbacd096145a3d7ff4be5c6c9f4fda978
Pedro Martins <76529868+martinsbrz@users.noreply.github.com>
2021-08-11 14:33:40 -0300

    Update CollectResultCandleFactory.cs

--- src/SC.FINANCIALMARKET/SC.FINANCIALMARKET.DOMAIN/Factories/CollectResultCandleFactory.cs
+++ src/SC.FINANCIALMARKET/SC.FINANCIALMARKET.DOMAIN/Factories/CollectResultCandleFactory.cs
@@ -46,7 +46,7 @@ namespace SC.FINANCIALMARKET.DOMAIN.Factories
                 int countLose = 0;
                 foreach (var candle in candles.Infos)
                 {
-                    if (candle.From.DateTime > DateTime.UtcNow)
+                    if (candle.From.DateTime.ToLocalTime() > DateTime.Now)
                     {
                         sinal.Resultado = "Aguardando...";
                         break;
